80508aa615eb41855e4ed5d818fcfca253f76e76
Damodar Lohani <dlohani48@gmail.com>
2017-10-14 08:38:15 +0545

    changing android package name

--- android/app/src/main/java/com/olohanitech/newsreader/MainActivity.java
+++ android/app/src/main/java/com/olohanitech/newsreader/MainActivity.java
@@ -1,4 +1,4 @@
-package com.newsreader;
+package com.olohanitech.newsreader;
 
 import com.facebook.react.ReactActivity;
 
--- android/app/src/main/java/com/olohanitech/newsreader/MainApplication.java
+++ android/app/src/main/java/com/olohanitech/newsreader/MainApplication.java
@@ -1,4 +1,4 @@
-package com.newsreader;
+package com.olohanitech.newsreader;
 
 import android.app.Application;
 
